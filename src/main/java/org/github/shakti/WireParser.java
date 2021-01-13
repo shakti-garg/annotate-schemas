e350dc46731c75e18e37bfe8f0fdae6c2eb9d44b
shakti-garg <shakti.garg@gmail.com>
2021-01-13 20:09:15 +0530

    refactor WireParser

--- src/main/java/org/github/shakti/WireParser.java
+++ src/main/java/org/github/shakti/WireParser.java
@@ -1,43 +1,22 @@
 package org.github.shakti;
 
+import com.squareup.wire.schema.Location;
+import com.squareup.wire.schema.internal.parser.*;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Collections;
-import com.squareup.wire.schema.Location;
-import com.squareup.wire.schema.ProtoFile;
-import com.squareup.wire.schema.Schema;
-import com.squareup.wire.schema.SchemaLoader;
-import com.squareup.wire.schema.internal.parser.FieldElement;
-import com.squareup.wire.schema.internal.parser.MessageElement;
-import com.squareup.wire.schema.internal.parser.OptionElement;
-import com.squareup.wire.schema.internal.parser.ProtoFileElement;
-import com.squareup.wire.schema.internal.parser.ProtoParser;
-import com.squareup.wire.schema.internal.parser.TypeElement;
-
-//import io.confluent.kafka.schemaregistry.ParsedSchema;
-//import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaReference;
-//import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema;
 
 public class WireParser {
-  private static final String DEFAULT_LOCATION = "src/main/proto";
+  private static final String DEFAULT_LOCATION = "/Users/shaktig/Projects/saxo/workspace/annotate-schemas";
 
   public static void main(String[] args) throws IOException {
     final String path = "src/main/resources/playground/v1/quote_sample.proto";
     final File schemaFile = new File(path);
 
-//      final SchemaLoader loader = new SchemaLoader();
-//      loader.addSource(file);
-//      final Schema schema = loader.load();
-//      final List<ProtoFile> protoFiles = schema.getProtoFiles();
-
-//    ProtobufSchema parsedSchema = (ProtobufSchema) parseProtobufSchema(
-//        new File("src/main/resources/playground/v1/quote_sample.proto"),
-//        Collections.emptyList());
-
     final String schemaString = new String(Files.readAllBytes(schemaFile.toPath()));
     final ProtoFileElement protoFileElement = ProtoParser.Companion
-        .parse(Location.get(""), schemaString);
+        .parse(Location.get(DEFAULT_LOCATION), schemaString);
 
     System.out.println("=====Canonical Schema======");
     System.out.println(protoFileElement.toSchema());
@@ -75,15 +54,4 @@ public class WireParser {
     }
   }
 
-//    private static ParsedSchema parseProtobufSchema(File schemaFile, List<SchemaReference> references) {
-//        try {
-//            return new ProtobufSchema(new String(Files.readAllBytes(schemaFile.toPath())),
-//                    references,
-//                    Collections.emptyMap(),
-//                    (Integer)null,
-//                    (String)null);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
 }
